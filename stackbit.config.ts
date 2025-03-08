7e93c683d2564b78f7bf1a26fe48923d6271095a
“mere” <github@zoltanb.co.uk>
2025-03-08 15:58:27 +0000

    fix: stackbit config

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -2,9 +2,9 @@ import { defineStackbitConfig } from "@stackbit/types";
 import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
-  stackbitVersion: "~0.6.0",
+  stackbitVersion: "~0.7.0",
   ssgName: "nextjs",
-  nodeVersion: "18",
+  nodeVersion: "20",
   sitemap: (options) => {
     return [
       {
